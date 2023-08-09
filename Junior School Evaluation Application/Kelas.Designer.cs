00d3bde711341ac0f324440ac302240494551eaf
Bagpopo01 <bagaspopo02@gmail.com>
2023-08-09 23:08:22 +0700

    update

--- Junior School Evaluation Application/Kelas.Designer.cs
+++ Junior School Evaluation Application/Kelas.Designer.cs
@@ -47,7 +47,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Tambah";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
@@ -90,7 +90,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Edit";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
