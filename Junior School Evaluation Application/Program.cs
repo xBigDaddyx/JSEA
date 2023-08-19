13ee65dc0760257cd5fc1fdd58e05b12bcc9f113
xBigDaddyx <yussupcazzanova97@gmail.com>
2023-08-19 21:21:20 +0700

    refetch

---
+++ Junior School Evaluation Application/Program.cs
@@ -0,0 +1,30 @@
+﻿using Junior_School_Evaluation_Application.Auth.Models;
+using Junior_School_Evaluation_Application.Auth.Services;
+using Junior_School_Evaluation_Application.Auth.Views;
+using System;
+using System.Windows.Forms;
+
+namespace Junior_School_Evaluation_Application
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Teachers teacherModel = new Teachers();
+
+            TeacherServices teacherService = new TeacherServices(teacherModel, DatabaseUtility.GetConnectionString());
+            Login loginForm = new Login(teacherService);
+
+            teacherService.Initialize(loginForm);
+
+            Application.Run(loginForm);
+        }
+    }
+}
